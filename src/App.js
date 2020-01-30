583f19f559f7eeb733c65ba416c5bc848eb66ad1
josh marinacci <joshua@marinacci.org>
2020-01-30 18:24:12 +0100

    enable fullscreen mode

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, {useState} from 'react'
+import React, {useRef, useState} from 'react'
 import './App.css'
-import {FillBox, Spacer, Toolbar} from './layout.js'
+import {Spacer, Toolbar} from './layout.js'
 import {TodoApp} from './todoapp.js'
 import {MailApp} from './mailapp.js'
-import {List, Mail, FileText, Maximize2} from "react-feather"
+import {FileText, List, Mail, Maximize2, Minimize2} from "react-feather"
 import {NotesApp} from './notesapp.js'
 
 function App() {
@@ -12,23 +12,29 @@ function App() {
     const switchMail = () => setApp('mail')
     const switchNotes = () => setApp('notes')
     let appcontent = ""
-    if(app === 'todo') appcontent = <TodoApp/>
-    if(app === 'mail') appcontent = <MailApp/>
-    if(app === 'notes') appcontent = <NotesApp/>
+    if (app === 'todo') appcontent = <TodoApp/>
+    if (app === 'mail') appcontent = <MailApp/>
+    if (app === 'notes') appcontent = <NotesApp/>
 
-    const enterFullscreen = () => {
-
+    const top = useRef()
+    const [full, setFull] = useState(false)
+    const toggleFullscreen = () => {
+        if (full) return document.exitFullscreen().then(() => setFull(false))
+        if (top.current) return top.current.requestFullscreen().then(() => setFull(true))
     }
 
-    return <FillBox>
+    let fsicon = full ? <Minimize2/> : <Maximize2/>
+
+    return <div className={'fillbox'} ref={top}>
         <Toolbar>
             <button onClick={switchTodo}><List/> todo</button>
             <button onClick={switchMail}><Mail/> mail</button>
             <button onClick={switchNotes}><FileText/> notes</button>
             <Spacer/>
-            <button disabled={true} onClick={enterFullscreen}><Maximize2/> full screen</button>
+            <button onClick={toggleFullscreen}>{fsicon} full screen</button>
         </Toolbar>
         {appcontent}
-    </FillBox>
+    </div>
 }
+
 export default App
