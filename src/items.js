8481588519ecfbaca4b33ac20e97632b7315b632
josh marinacci <joshua@marinacci.org>
2020-01-30 18:25:10 +0100

    code reformatting

--- src/items.js
+++ src/items.js
@@ -1,5 +1,5 @@
 import React, {useContext, useEffect, useRef, useState} from 'react'
-import {ActionContext, useActionScope} from './actions.js'
+import {useActionScope} from './actions.js'
 import {
     FocusContext,
     GenericListView,
@@ -11,44 +11,44 @@ import {
     VBox
 } from './layout.js'
 import {StorageContext, useObjectUpdate, useQuery} from './storage.js'
-import {Star, File} from 'react-feather'
+import {File, Star} from 'react-feather'
 
-const getProjectTitle = (storage,item) => {
-    const proj = storage.find('projects',(proj)=>proj.id===item.project)
-    if(proj) return proj.title
+const getProjectTitle = (storage, item) => {
+    const proj = storage.find('projects', (proj) => proj.id === item.project)
+    if (proj) return proj.title
     return ""
 }
 
 const ItemEditPanel = ({item, setEditing}) => {
     const title = useRef()
     const storage = useContext(StorageContext)
-    const [setProp] = useObjectUpdate(storage,'items',item)
+    const [setProp] = useObjectUpdate(storage, 'items', item)
     const toggleCompleted = () => {
-        setProp('completed',!item.completed)
-        setProp('completedTimestamp',Date.now())
+        setProp('completed', !item.completed)
+        setProp('completedTimestamp', Date.now())
     }
-    const toggleToday = () => setProp('today',!item.today)
-    const editTitle = (e) => setProp('title',e.target.value)
-    const editNotes = (e) => setProp('notes',e.target.value)
+    const toggleToday = () => setProp('today', !item.today)
+    const editTitle = (e) => setProp('title', e.target.value)
+    const editNotes = (e) => setProp('notes', e.target.value)
     const endEditing = () => setEditing(false)
 
-    const handlers = useActionScope('edit-item',{
-        'exit-edit-item': endEditing,
+    const handlers = useActionScope('edit-item', {
+        'exit-edit-item': endEditing
     })
 
-    const [projects] = useState(()=> storage.createQuery('projects',(p)=>!p.special))
+    const [projects] = useState(() => storage.createQuery('projects', (p) => !p.special))
 
 
     //focus when first opening
-    useEffect(()=>{
-        if(title.current) title.current.focus()
-    },[item])
+    useEffect(() => {
+        if (title.current) title.current.focus()
+    }, [item])
 
     return <div className={"edit-panel"} onKeyDown={handlers.onKeyDown}>
         <HBox>
             <input type="checkbox" checked={item.completed} onChange={toggleCompleted}/>
-            <input type="text" value={item.title} className="grow" onChange={editTitle} ref={title} onKeyDown={(e)=>{
-                if(e.key === 'Enter') endEditing()
+            <input type="text" value={item.title} className="grow" onChange={editTitle} ref={title} onKeyDown={(e) => {
+                if (e.key === 'Enter') endEditing()
             }}/>
         </HBox>
         <HBox>
@@ -58,38 +58,38 @@ const ItemEditPanel = ({item, setEditing}) => {
             <label>today</label>
             <input type={'checkbox'} checked={item.today} onChange={toggleToday}/>
             <PopupButton
-                getItems={()=> projects.results()}
-                stringify={(item)=>item.title}
-                itemSelected={(item)=> setProp('project',item.id)}
-            >{getProjectTitle(storage,item)}</PopupButton>
+                getItems={() => projects.results()}
+                stringify={(item) => item.title}
+                itemSelected={(item) => setProp('project', item.id)}
+            >{getProjectTitle(storage, item)}</PopupButton>
             <Spacer/>
             <button onClick={endEditing}>done</button>
         </HBox>
     </div>
 }
 
-const ItemViewItem = ({item, setEditing, focusName, selected})=>{
+const ItemViewItem = ({item, setEditing, focusName, selected}) => {
     const hbox = useRef()
     const storage = useContext(StorageContext)
-    const [setProp] = useObjectUpdate(storage,'items',item)
-    const toggleCompleted = () => setProp('completed',!item.completed)
-    useAutofocusRefWhenSelected(hbox,selected,focusName)
-    const handlers = useActionScope('item',{
-        'edit-item': ()=>  setEditing(true),
-        'exit-edit-item': ()=>setEditing(false),
+    const [setProp] = useObjectUpdate(storage, 'items', item)
+    const toggleCompleted = () => setProp('completed', !item.completed)
+    useAutofocusRefWhenSelected(hbox, selected, focusName)
+    const handlers = useActionScope('item', {
+        'edit-item': () => setEditing(true),
+        'exit-edit-item': () => setEditing(false)
     })
 
     const cls = makeClassNames({
-        'hbox':true,
-        'todo-item':true,
-        'deleted':item.deleted,
+        'hbox': true,
+        'todo-item': true,
+        'deleted': item.deleted
     })
     return <div ref={hbox}
                 tabIndex={0}
                 className={cls}
                 onKeyDown={handlers.onKeyDown}
-                onDoubleClick={()=>setEditing(true)}
-        >
+                onDoubleClick={() => setEditing(true)}
+    >
         <TodayIndicator item={item}/>
         <input type="checkbox" checked={item.completed} onChange={toggleCompleted}/>
         <VBox>
@@ -97,28 +97,28 @@ const ItemViewItem = ({item, setEditing, focusName, selected})=>{
                 <span className={'title'}>{item.title}</span>
                 <NotesIndicator item={item}/>
             </b>
-            <i>{getProjectTitle(storage,item)}</i>
+            <i>{getProjectTitle(storage, item)}</i>
         </VBox>
     </div>
 }
-const TodayIndicator = ({item})=>{
-    if(item.today) {
+const TodayIndicator = ({item}) => {
+    if (item.today) {
         return <Star size={16}/>
     } else {
         return <b className={'empty-icon'}></b>
     }
 }
 
-const NotesIndicator = ({item})=>{
-    if(item.notes) {
+const NotesIndicator = ({item}) => {
+    if (item.notes) {
         return <span className={"notes-indicator"}><File size={12}/></span>
     } else {
         return <b className={'empty-icon'}></b>
     }
 }
-const TodoItemView = ({item, focusName, selected})=>{
+const TodoItemView = ({item, focusName, selected}) => {
     const [editing, setEditing] = useState(false)
-    if(editing) {
+    if (editing) {
         return <ItemEditPanel item={item} setEditing={setEditing}/>
     } else {
         return <ItemViewItem item={item} setEditing={setEditing} focusName={focusName} selected={selected}/>
@@ -132,79 +132,79 @@ export const ItemsListView = ({query, project}) => {
         const item = storage.insert('items', {
             title: 'empty item',
             tags: [],
-            project:project.id,
-            completed:false,
-            today:(project.special && project.title === 'today'),
-            notes:"",
-            deleted:false,
+            project: project.id,
+            completed: false,
+            today: (project.special && project.title === 'today'),
+            notes: "",
+            deleted: false
         })
         setSel(item)
     }
     const fm = useContext(FocusContext)
     const [items] = useQuery(query)
-    const handlers = useActionScope('list',{
+    const handlers = useActionScope('list', {
         'focus-prev-master': () => {
             fm.setMasterFocus('projects')
         },
         'focus-next-master': () => {
         },
-        'add-item-to-target-list':addItem,
-        'toggle-completed': ()=>{
-            storage.update('items',sel,'completed',!sel.completed)
+        'add-item-to-target-list': addItem,
+        'toggle-completed': () => {
+            storage.update('items', sel, 'completed', !sel.completed)
         },
         'toggle-today': () => {
-            storage.update('items',sel,'today',!sel.today)
+            storage.update('items', sel, 'today', !sel.today)
         },
-        'delete-item':(e)=> {
-            if(e.target.classList.contains('todo-item')) {
-                storage.update('items',sel,'deleted',!sel.deleted)
+        'delete-item': (e) => {
+            if (e.target.classList.contains('todo-item')) {
+                storage.update('items', sel, 'deleted', !sel.deleted)
             }
         },
-        'shift-selection-prev':() => {
-            if(project.special) return console.log("cannot change sort in a special")
+        'shift-selection-prev': () => {
+            if (project.special) return console.log("cannot change sort in a special")
             const index = items.indexOf(sel)
-            const newIndex = index-1
-            console.log("moving the selection up from",index,'to',newIndex, sel)
-            if(newIndex < 0) return console.log("can't move up past the start")
+            const newIndex = index - 1
+            console.log("moving the selection up from", index, 'to', newIndex, sel)
+            if (newIndex < 0) return console.log("can't move up past the start")
             const prev = items[newIndex]
-            console.log("prev is",prev)
-            const prevprev = items[newIndex-1]
-            console.log("prevprev",prevprev)
+            console.log("prev is", prev)
+            const prevprev = items[newIndex - 1]
+            console.log("prevprev", prevprev)
             let sortA = prev.sortOrder
             let sortB = 0
-            if(prevprev) sortB = prevprev.sortOrder
-            const newOrder = (sortA + sortB)/2
-            console.log("the new order is",newOrder)
-            storage.update('items',sel,'sortOrder',newOrder)
+            if (prevprev) sortB = prevprev.sortOrder
+            const newOrder = (sortA + sortB) / 2
+            console.log("the new order is", newOrder)
+            storage.update('items', sel, 'sortOrder', newOrder)
         },
-        'shift-selection-next':() => {
-            if(project.special) return console.log("cannot change sort in a special")
+        'shift-selection-next': () => {
+            if (project.special) return console.log("cannot change sort in a special")
             const index = items.indexOf(sel)
-            const newIndex = index+1
-            console.log('moving selection',sel)
-            if(newIndex > items.length-1) return console.log("cannot move down past the end")
+            const newIndex = index + 1
+            console.log('moving selection', sel)
+            if (newIndex > items.length - 1) return console.log("cannot move down past the end")
             const next = items[newIndex]
             let sortA = next.sortOrder
             let sortB = Number.MAX_SAFE_INTEGER
-            const nextnext = items[newIndex+1]
-            if(nextnext) sortB = nextnext.sortOrder
-            console.log("will be after",nextnext)
-            const newOrder = (sortA + sortB)/2
-            console.log("the new order is",newOrder)
-            storage.update('items',sel,'sortOrder',newOrder)
+            const nextnext = items[newIndex + 1]
+            if (nextnext) sortB = nextnext.sortOrder
+            console.log("will be after", nextnext)
+            const newOrder = (sortA + sortB) / 2
+            console.log("the new order is", newOrder)
+            storage.update('items', sel, 'sortOrder', newOrder)
         }
 
     })
     let emptyTrash = ""
     const emptyTrashAction = () => {
-        storage.delete('items',(it)=>it.deleted)
+        storage.delete('items', (it) => it.deleted)
     }
     let addButton = <button onClick={addItem}>add</button>
-    if(project && project.special && project.title === 'trash') {
+    if (project && project.special && project.title === 'trash') {
         emptyTrash = <button onClick={emptyTrashAction}>empty</button>
         addButton = ""
     }
-    return <div  onKeyDown={handlers.onKeyDown} className={'items-list-view'}>
+    return <div onKeyDown={handlers.onKeyDown} className={'items-list-view'}>
         <GenericListView
             ItemTemplate={TodoItemView}
             selectedItem={sel}
--- src/projects.js
+++ src/projects.js
@@ -1,11 +1,11 @@
 import React, {useContext, useEffect, useRef, useState} from 'react'
 import {StorageContext, useQuery} from './storage.js'
 import {useActionScope} from './actions.js'
-import {FocusContext, GenericListView, makeClassNames, Spacer, useAutofocusRefWhenSelected} from './layout.js'
-import {Star, Square, Trash2, CheckSquare, Coffee} from "react-feather"
+import {FocusContext, GenericListView, Spacer, useAutofocusRefWhenSelected} from './layout.js'
+import {CheckSquare, Coffee, Square, Star, Trash2} from "react-feather"
 
 
-const ProjectItemView = ({item,selected,focusName})=> {
+const ProjectItemView = ({item, selected, focusName}) => {
     const project = item
     const [editing, setEditing] = useState(false)
     const storage = useContext(StorageContext)
@@ -13,41 +13,41 @@ const ProjectItemView = ({item,selected,focusName})=> {
     const div = useRef()
 
     const fm = useContext(FocusContext)
-    const startEditing = ()=>{
-        if(item.special) return
+    const startEditing = () => {
+        if (item.special) return
         setEditing(true)
         fm.pushMasterFocus('edit')
     }
 
-    useAutofocusRefWhenSelected(div,selected,focusName)
+    useAutofocusRefWhenSelected(div, selected, focusName)
 
-    useEffect(()=> {
+    useEffect(() => {
         if (input.current) input.current.focus()
-    },[editing])
+    }, [editing])
 
     let icon = <Square/>
-    if(project.special && project.title === 'today')  icon = <Star/>
-    if(project.special && project.title === 'trash')  icon = <Trash2/>
-    if(project.special && project.title === 'completed')icon = <CheckSquare/>
-    if(project.special && project.title === 'everything')icon = <Coffee/>
-    if(editing) {
+    if (project.special && project.title === 'today') icon = <Star/>
+    if (project.special && project.title === 'trash') icon = <Trash2/>
+    if (project.special && project.title === 'completed') icon = <CheckSquare/>
+    if (project.special && project.title === 'everything') icon = <Coffee/>
+    if (editing) {
         return (<div className={'hbox project-item'}> {icon}
             <input ref={input} type={'text'} className={'grow'}
-                   value={project.title} onChange={(e)=>{
-                storage.update("projects",project,'title',e.target.value)
+                   value={project.title} onChange={(e) => {
+                storage.update("projects", project, 'title', e.target.value)
             }}
-                   onKeyDown={(e)=>{
-                       if(e.key === 'Enter') {
+                   onKeyDown={(e) => {
+                       if (e.key === 'Enter') {
                            fm.popMasterFocus()
                            setEditing(false)
                        }
                    }}
             />
-            </div>)
+        </div>)
     } else {
         return <div ref={div} tabIndex={0} className={'hbox project-item'}
-                    onKeyDown={(e)=>{
-                        if(e.key === 'Enter') startEditing()
+                    onKeyDown={(e) => {
+                        if (e.key === 'Enter') startEditing()
                     }}
                     onDoubleClick={startEditing}>
             {icon}
@@ -56,66 +56,66 @@ const ProjectItemView = ({item,selected,focusName})=> {
     }
 }
 
-export const ProjectsListView = ({selectedProject, setSelectedProject, nextFocusTarget="items"})=> {
+export const ProjectsListView = ({selectedProject, setSelectedProject, nextFocusTarget = "items"}) => {
     const storage = useContext(StorageContext)
-    const [apq] = useState(()=> storage.createQuery(
+    const [apq] = useState(() => storage.createQuery(
         'projects', // only projects
-        ()=>true, // all projects
-        (a,b)=>a.sortOrder-b.sortOrder), // sort by the sort order
+        () => true, // all projects
+        (a, b) => a.sortOrder - b.sortOrder) // sort by the sort order
     )
     const [projects] = useQuery(apq)
     const fm = useContext(FocusContext)
-    const handlers = useActionScope('list',{
+    const handlers = useActionScope('list', {
         'focus-prev-master': () => {
         },
         'focus-next-master': () => {
             fm.setMasterFocus(nextFocusTarget)
         },
-        'move-selection-prev':()=>{
+        'move-selection-prev': () => {
             const index = projects.indexOf(selectedProject)
-            if(index > 0) setSelectedProject(projects[index-1])
+            if (index > 0) setSelectedProject(projects[index - 1])
         },
-        'move-selection-next':()=>{
+        'move-selection-next': () => {
             const index = projects.indexOf(selectedProject)
-            if(index < projects.length-1) setSelectedProject(projects[index+1])
+            if (index < projects.length - 1) setSelectedProject(projects[index + 1])
         },
-        'shift-selection-prev':() => {
-            if(selectedProject.special) {
+        'shift-selection-prev': () => {
+            if (selectedProject.special) {
                 console.log("cannot move special ones")
                 return
             }
             const index = projects.indexOf(selectedProject)
-            const newIndex = index-1
-            console.log("moving the selection up from",index,'to',newIndex, selectedProject)
-            if(newIndex <= 0) return console.log("can't move up past the start")
+            const newIndex = index - 1
+            console.log("moving the selection up from", index, 'to', newIndex, selectedProject)
+            if (newIndex <= 0) return console.log("can't move up past the start")
             const prev = projects[newIndex]
-            console.log("prev is",prev)
-            if(prev.special) {
+            console.log("prev is", prev)
+            if (prev.special) {
                 console.log("cant move before a special")
                 return
             }
-            const prevprev = projects[newIndex-1]
-            console.log("prevprev",prevprev)
-            const newOrder = (prev.sortOrder + prevprev.sortOrder)/2
-            console.log("the new order is",newOrder)
-            storage.update('projects',selectedProject,'sortOrder',newOrder)
+            const prevprev = projects[newIndex - 1]
+            console.log("prevprev", prevprev)
+            const newOrder = (prev.sortOrder + prevprev.sortOrder) / 2
+            console.log("the new order is", newOrder)
+            storage.update('projects', selectedProject, 'sortOrder', newOrder)
         },
-        'shift-selection-next':() => {
-            if(selectedProject.special) {
+        'shift-selection-next': () => {
+            if (selectedProject.special) {
                 console.log("cannot move special ones")
                 return
             }
             const index = projects.indexOf(selectedProject)
-            const newIndex = index+1
-            console.log('moving selection',selectedProject)
-            if(newIndex > projects.length-1) return console.log("cannot move down past the end")
+            const newIndex = index + 1
+            console.log('moving selection', selectedProject)
+            if (newIndex > projects.length - 1) return console.log("cannot move down past the end")
             const next = projects[newIndex]
-            if(next.special) return console.log("can't move above a special")
-            const nextnext = projects[newIndex+1]
-            console.log("will be after",nextnext)
-            const newOrder = (next.sortOrder + nextnext.sortOrder)/2
-            console.log("the new order is",newOrder)
-            storage.update('projects',selectedProject,'sortOrder',newOrder)
+            if (next.special) return console.log("can't move above a special")
+            const nextnext = projects[newIndex + 1]
+            console.log("will be after", nextnext)
+            const newOrder = (next.sortOrder + nextnext.sortOrder) / 2
+            console.log("the new order is", newOrder)
+            storage.update('projects', selectedProject, 'sortOrder', newOrder)
         }
     })
     return <div onKeyDown={handlers.onKeyDown} className={'projects-list-view'}>
