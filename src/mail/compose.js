790f899e280824afbd13b018fc6f0bd8db4789c1
josh marinacci <joshua@marinacci.org>
2020-02-07 08:58:28 -0800

    update compose to useDraft

--- src/mail/compose.js
+++ src/mail/compose.js
@@ -1,19 +1,10 @@
-import React, {useContext, useState} from 'react'
-import {StorageContext, useQuery} from '../common/storage2.js'
+import React from 'react'
+import {useDraft} from '../common/storage2.js'
 import {useActionScope} from '../common/actions.js'
 import {HBox, VBox} from '../common/layout.js'
 
 export function ComposingMailView({mail, done}) {
-    const storage = useContext(StorageContext)
-    const [q] = useState(() => {
-        return storage.createQuery('mails', (m) => m === mail)
-    })
-    const [realmailarray] = useQuery(q)
-
-    const editedTo = (e) => storage.update('message',mail,'receiver', e.target.value)
-    const editedSubject = (e) => storage.update('message',mail,'subject', e.target.value)
-    const editedBody = (e) => storage.update('message',mail,'body', e.target.value)
-
+    const [draft, update, save] = useDraft(mail)
     const handlers = useActionScope('compose', {
         'send-mail': done
     })
@@ -21,18 +12,13 @@ export function ComposingMailView({mail, done}) {
     return <VBox className={'composing-mail-view'} onKeyDown={handlers.onKeyDown}>
         <HBox>
             <label>to</label>
-            <input type="text" value={mail.receiver} className={'grow'}
-                   onChange={editedTo}/>
+            <input type="text" value={draft.receiver} className={'grow'}  onChange={(e)=>update('receiver',e.target.value)}/>
         </HBox>
         <HBox>
             <label>Subject</label>
-            <input type="text" value={mail.subject} className='grow'
-                   onChange={editedSubject}
-            />
+            <input type="text" value={draft.subject} className='grow' onChange={(e)=>update('subject',e.target.value)}/>
         </HBox>
-        <textarea className={'grow stretch'} value={mail.body}
-                  onChange={editedBody}
-        />
+        <textarea className={'grow stretch'} value={draft.body} onChange={(e)=>update('body',e.target.value)}/>
         <HBox>
             <button onClick={done}>save for later</button>
             <button onClick={done}>send now</button>
