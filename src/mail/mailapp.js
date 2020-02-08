46040abd809a4cd9ffd4bc8aff6ab03d47b3294e
Josh Marinacci <joshua@marinacci.org>
2020-02-08 15:20:27 -0800

    move settings add color themes

--- src/mail/mailapp.js
+++ src/mail/mailapp.js
@@ -27,6 +27,7 @@ import {StorageContext, Storage} from '../common/storage2.js'
 import {ReadingMailView} from './read.js'
 import {ComposingMailView} from './compose.js'
 import {FoldersListView, MailsListView} from './folders.js'
+import {SettingsDialog} from './settings.js'
 
 const storage = new Storage()
 const FOLDER = storage.defineTable({
@@ -128,42 +129,6 @@ export const MailApp = () => {
     </ActionContext.Provider>
 }
 
-const SPACING = {
-    'comfortable': '1.0rem',
-    'tight':'0.5rem',
-}
-const SettingsDialog = () => {
-    const dm = useContext(DialogContext)
-    const css = CSS({
-        dialog:'true'
-    })
-    const hide = () => {
-        dm.hide()
-    }
-
-    const [spacing,setSpacing] = useState('comfortable')
-    return <div className={css}>
-        <header>Settings</header>
-        <div className='body'>
-            <label>change your theme</label>
-            <HBox>
-            <label>spacing</label>
-            <select value={spacing} onChange={(e)=>{
-                const v = e.target.value
-                document.documentElement.style.setProperty('--spacing',SPACING[v])
-                setSpacing(v)
-            }}>
-                <option value={"comfortable"}>comfortable</option>
-                <option value={"tight"}>tight</option>
-            </select>
-            </HBox>
-        </div>
-        <footer>
-            <Spacer/>
-            <button onClick={hide}>done</button>
-        </footer>
-    </div>
-}
 
 
 const MailAppContent = () => {
---
+++ src/mail/settings.js
@@ -0,0 +1,78 @@
+import React, {useContext, useState} from 'react'
+import {CSS, DialogContext, HBox, Spacer} from '../common/layout.js'
+
+const SPACING = {
+    'comfortable': '1.0rem',
+    'tight':'0.5rem',
+}
+const COLOR_THEME = {
+    'default': {
+        '--tool-bg': '#aaaaaa',
+        '--tool-fg': '#333',
+        '--content-bg': '#dddddd',
+        '--focus-bg': '#ffffff',
+        '--active-bg': 'aquamarine'
+    },
+    'desert-night':{
+        '--tool-bg': '#765D69',
+        '--tool-fg': '#f0f0f0',
+        '--content-bg': '#FCD0BA',
+        '--focus-bg': '#FEFAD4',
+        '--active-bg': '#F1828D',
+    },
+    'gradient': {
+        '--tool-bg':'linear-gradient(to right, #cfd9df, #e2ebf0)',
+        '--tool-fg':'#333',
+        '--active-bg':'linear-gradient(to right, aqua, aquamarine)',
+        '--content-bg':'#f0f0f0',
+    }
+}
+export const SettingsDialog = () => {
+    const dm = useContext(DialogContext)
+    const css = CSS({
+        dialog:'true'
+    })
+    const hide = () => {
+        dm.hide()
+    }
+
+    const [spacing,setSpacing] = useState('comfortable')
+    const [color, setColor] = useState('default')
+    return <div className={css}>
+        <header>Settings</header>
+        <div className='body'>
+            <label>change your theme</label>
+            <HBox>
+                <label>spacing</label>
+                <select value={spacing} onChange={(e)=>{
+                    const v = e.target.value
+                    document.documentElement.style.setProperty('--spacing',SPACING[v])
+                    setSpacing(v)
+                }}>
+                    <option value={"comfortable"}>comfortable</option>
+                    <option value={"tight"}>tight</option>
+                </select>
+            </HBox>
+            <HBox>
+                <label>color</label>
+                <select value={color} onChange={e => {
+                    const v = e.target.value
+                    const theme = COLOR_THEME[v]
+                    Object.keys(theme).forEach(key => {
+                        document.documentElement.style.setProperty(key,theme[key])
+                    })
+                    setColor(v)
+                }}>
+                    {Object.keys(COLOR_THEME).map(key => {
+                        console.log('theme',key)
+                        return <option key={key} value={key}>{key}</option>
+                    })}
+                </select>
+            </HBox>
+        </div>
+        <footer>
+            <Spacer/>
+            <button onClick={hide}>done</button>
+        </footer>
+    </div>
+}
